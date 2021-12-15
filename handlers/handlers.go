d2b6e7ee4a21876318f0d89293dd92845e823709
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-12-15 13:53:36 +0200

    Add addUser handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
 	"strconv"
 	"webApp/userInfo/model"
 )
@@ -27,6 +28,7 @@ func IndexFunc(w http.ResponseWriter, r *http.Request) {
 
 //handler to show user with id input
 func ShowUserFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method)
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("templates/showUserPage.html")
 		t.Execute(w, nil)
@@ -38,6 +40,7 @@ func ShowUserFunc(w http.ResponseWriter, r *http.Request) {
 		var alUsrs model.AllUsers
 		file, err := os.OpenFile("list.json", os.O_RDONLY, 0666)
 		checkError(err)
+		defer file.Close()
 		b, err := ioutil.ReadAll(file)
 		checkError(err)
 		json.Unmarshal(b, &alUsrs.Users)
@@ -69,4 +72,82 @@ func ShowUser(w http.ResponseWriter, r *http.Request) {
 
 func NotSuccededShow(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "templates/notSuccededShow.html")
+}
+
+//function to check correct user adding input (regular expression and non-empty field input)
+func checkFormValue(w http.ResponseWriter, r *http.Request, forms ...string) (res bool, errStr string) {
+	for _, form := range forms {
+		reg, _ := regexp.Compile("^[a-zA-Z]+$")
+		m := reg.MatchString(r.FormValue(form))
+
+		if r.FormValue(form) == "" {
+			return false, "All forms must be completed"
+		}
+		if !m {
+			return false, "Use only english letters if firstname,lastname forms"
+		}
+
+	}
+	return true, ""
+}
+
+//function to add user
+func AddNewUserFunc(w http.ResponseWriter, r *http.Request) {
+
+	//creating new instance and checking method
+	newUser := &model.User{}
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("templates/addNewUser.html")
+		t.Execute(w, nil)
+
+	} else {
+		resBool, errStr := checkFormValue(w, r, "firstname", "lastname")
+		if !resBool {
+			t, err := template.ParseFiles("templates/notSucceded.html")
+			checkError(err)
+			t.Execute(w, errStr)
+
+			return
+		}
+		newUser.FirstName = r.FormValue("firstname")
+		newUser.LastName = r.FormValue("lastname")
+		var err error
+		newUser.Balance, err = strconv.ParseFloat(r.FormValue("balance"), 64)
+		checkError(err)
+
+		//open file
+		file, err := os.OpenFile("list.json", os.O_RDWR, 0644)
+		checkError(err)
+		defer file.Close()
+
+		//read file and unmarshall json file to slice of users
+		b, err := ioutil.ReadAll(file)
+		checkError(err)
+		var alUsrs model.AllUsers
+		err = json.Unmarshal(b, &alUsrs.Users)
+		checkError(err)
+		max := 0
+
+		//generation of id(last id at the json file+1)
+		for _, usr := range alUsrs.Users {
+			if usr.Id > max {
+				max = usr.Id
+			}
+		}
+		id := max + 1
+		newUser.Id = id
+
+		//appending newUser to slice of all Users and rewrite json file
+		alUsrs.Users = append(alUsrs.Users, newUser)
+		newUserBytes, err := json.MarshalIndent(&alUsrs.Users, "", " ")
+		checkError(err)
+		ioutil.WriteFile("list.json", newUserBytes, 0666)
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+
+	}
+}
+
+//serving file with error (add function:empty field input or uncorrect input)
+func NotSucceded(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/notSucceded.html")
 }
