f067c46dec5adcf4cb20dbc505389721d915b08d
Mohamed-Magdey <ragabmohame6@gmail.com>
2021-12-15 12:17:02 +0200

    Use show user handlers

--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
-
 	http.HandleFunc("/", handlers.IndexFunc)
+
+	http.HandleFunc("/showuser/show", handlers.ShowUserFunc)
+	http.HandleFunc("/showuser/", handlers.ShowUser)
+	http.HandleFunc("/showuser/notsuccededshow/", handlers.NotSuccededShow)
 
 	http.ListenAndServe(":8080", nil)
 }
